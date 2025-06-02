3c802a865fbede176dcd218b714b85be4d297ff6
joelito <armandojoel2004@gmail.com>
2025-06-02 14:53:17 -0600

    terminando contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://joelitorepos.github.io',
+  base: "/mi-portfolio/",
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
