4aef8e7b908cadbe5fdc7bff526b0f343fb6a353
joelito <armandojoel2004@gmail.com>
2025-06-02 20:36:36 -0600

    arreglando estilos nuevo intento

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://joelitorepos.github.io',
-  base: "/mi-portafolio/",
-  output: "static",
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  base: '/mi-portafolio/',
+  output: 'static',
+  integrations: [tailwind()]
 });
